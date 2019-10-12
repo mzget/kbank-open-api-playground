600b26540b9691a0bcffcbf0876b18c54eac7fdd
Nattapon Rattajak <rattajak.n@gmail.com>
2019-10-12 22:01:25 +0700

    debug:

---
+++ pokemon-on-fire/app/pages/index.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Head from "next/head";
+import styled from "styled-components";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import ListSubheader from "@material-ui/core/ListSubheader";
+
+import MediaCard from "../src/components/MediaCard";
+import { Pokedex } from "../src/mock/pokedex";
+
+function Home(props: any) {
+  let [column, setColumn] = React.useState(2);
+  React.useEffect(() => {
+    console.log("Home Page");
+    let viewPort = document.getElementById("App-Container");
+    if (viewPort) {
+      let cardWidth = 280;
+      let column = viewPort.clientWidth / cardWidth;
+      setColumn(Math.floor(column));
+    }
+  }, [setColumn]);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <StyledShop className="StyledShop">
+        <StyledGridList cellHeight={"auto"} cols={column}>
+          <GridListTile
+            key="Subheader"
+            cols={column}
+            style={{ height: "auto" }}
+          >
+            <ListSubheader component="div">Choose your pokemons</ListSubheader>
+          </GridListTile>
+          {Pokedex.map(v => (
+            <GridListTile key={v.key}>
+              <MediaCard key={v.key} pokemon={v} />
+            </GridListTile>
+          ))}
+        </StyledGridList>
+      </StyledShop>
+    </React.Fragment>
+  );
+}
+
+export default Home;
+
+const StyledShop = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const StyledGridList = styled(GridList)`
+  && {
+    width: 100%;
+  }
+`;
---
+++ pokemon-on-fire/app/src/react-kpayment/KPayment.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+type KPaymentScriptProps = {
+  scriptUrl: string;
+  apiKey: string;
+  amount: string;
+  currency: string;
+  paymentMethods: "card" | "qr";
+  shopName: string;
+};
+type KPaymentProps = {
+  formAction: string;
+  attrs: KPaymentScriptProps;
+  onFinish?(result: string): void;
+  onError?(message: string): void;
+  onProcess?(): void;
+  debug?: boolean;
+};
+
+export default function KPayment(props: KPaymentProps) {
+  let {
+    attrs,
+    formAction,
+    onFinish,
+    onError,
+    onProcess,
+    debug = false,
+  } = props;
+
+  async function formSubmit(event) {
+    event.preventDefault();
+
+    let formData = new FormData(event.target);
+    let token = formData.get("token");
+    let paymentMethods = formData.get("paymentMethods");
+    let saveCard = formData.get("saveCard");
+
+    let data = {
+      token,
+      paymentMethods,
+      saveCard,
+    };
+    let url = formAction;
+    const resp = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (resp.ok) {
+      const result = await resp.json();
+      if (onFinish) onFinish(result);
+    } else {
+      const result = await resp.json();
+      if (onError) onError(JSON.stringify(result));
+    }
+
+    if (onProcess) onProcess();
+  }
+
+  React.useEffect(() => {
+    const script = document.createElement("script");
+    script.src = attrs.scriptUrl;
+    script.setAttribute("data-apikey", attrs.apiKey);
+    script.setAttribute("data-amount", attrs.amount);
+    script.setAttribute("data-currency", attrs.currency);
+    script.setAttribute("data-payment-methods", attrs.paymentMethods);
+    script.setAttribute("data-name", attrs.shopName);
+    script.type = "text/javascript";
+    script.async = true;
+    script.onload = ev => {
+      if (checkoutForm) {
+        if (debug) {
+          console.log("payment-container create new");
+        }
+        (window as any).KPayment.create();
+      }
+    };
+    let checkoutForm = document.getElementById(`checkout-form`);
+    if (checkoutForm) {
+      checkoutForm.appendChild(script);
+      checkoutForm.addEventListener("submit", formSubmit);
+    }
+
+    return () => {
+      if (checkoutForm) {
+        checkoutForm.removeChild(script);
+        checkoutForm.removeEventListener("submit", formSubmit);
+        let paymentContainer = document.querySelector(".payment-container");
+        if (paymentContainer) {
+          document.body.removeChild(paymentContainer);
+        }
+      }
+    };
+  }, []);
+
+  return <form id={`checkout-form`}></form>;
+}
